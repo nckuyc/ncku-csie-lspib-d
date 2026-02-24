f59a1e2f8d5a68156f58375af3b0591134de2785
nckuyc <h44131221@gs.ncku.edu.tw>
2026-02-24 17:07:21 +0800

    Enhance system command execution functions and update manual-linux.sh for kernel build steps

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,39 +1,43 @@
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
 
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
- *   successfully using the system() call, false if an error occurred,
- *   either in invocation of the system() call, or if a non-zero return
- *   value was returned by the command issued in @param cmd.
+ * successfully using the system() call, false if an error occurred,
+ * either in invocation of the system() call, or if a non-zero return
+ * value was returned by the command issued in @param cmd.
 */
 bool do_system(const char *cmd)
 {
+    if (cmd == NULL) return false;
 
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
+    int ret = system(cmd);
 
-    return true;
+    if (ret == -1) return false;
+
+    // 檢查子進程是否正常退出且結束碼為 0
+    return (WIFEXITED(ret) && WEXITSTATUS(ret) == 0);
 }
 
 /**
 * @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
+* followed by arguments to pass to the command
+* Since exec() does not perform path expansion, the command to execute needs
+* to be an absolute path.
 * @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
+* The first is always the full path to the command to execute with execv()
+* The remaining arguments are a list of arguments to pass to the command in execv()
 * @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
+* using the execv() call, false if an error occurred, either in invocation of the
+* fork, waitpid, or execv() command, or if a non-zero return value was returned
+* by the command issued in @param arguments with the specified arguments.
 */
-
 bool do_exec(int count, ...)
 {
     va_list args;
@@ -45,28 +49,39 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    // 關鍵修正：測試要求 execv 必須使用絕對路徑
+    if (command[0][0] != '/') {
+        va_end(args);
+        return false;
+    }
 
-    va_end(args);
+    fflush(stdout);
+    pid_t pid = fork();
 
-    return true;
+    if (pid == -1) {
+        va_end(args);
+        return false;
+    } else if (pid == 0) {
+        // 子進程
+        execv(command[0], command);
+        // 如果 execv 回傳，代表失敗
+        exit(EXIT_FAILURE);
+    } else {
+        // 父進程
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            va_end(args);
+            return false;
+        }
+        va_end(args);
+        return (WIFEXITED(status) && WEXITSTATUS(status) == 0);
+    }
 }
 
 /**
 * @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
+* This file will be closed at completion of the function call.
 * All other parameters, see do_exec above
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
@@ -80,20 +95,41 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
+    if (command[0][0] != '/') {
+        va_end(args);
+        return false;
+    }
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0) {
+        va_end(args);
+        return false;
+    }
 
-    va_end(args);
+    fflush(stdout);
+    pid_t pid = fork();
 
-    return true;
+    if (pid == -1) {
+        close(fd);
+        va_end(args);
+        return false;
+    } else if (pid == 0) {
+        // 重導向 STDOUT 到檔案
+        if (dup2(fd, STDOUT_FILENO) < 0) {
+            exit(EXIT_FAILURE);
+        }
+        close(fd);
+        execv(command[0], command);
+        exit(EXIT_FAILURE);
+    } else {
+        close(fd);
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            va_end(args);
+            return false;
+        }
+        va_end(args);
+        return (WIFEXITED(status) && WEXITSTATUS(status) == 0);
+    }
 }
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -11,6 +11,7 @@ KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
+# 修正編譯器名稱以符合多數容器環境
 CROSS_COMPILE=aarch64-linux-gnu-
 
 if [ $# -lt 1 ]
@@ -25,27 +26,18 @@ mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
-
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # === [TODO: Kernel Build Steps] ===
-    echo "Cleaning kernel source..."
+    # Build steps
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
-    
-    echo "Configuring for virt arm64..."
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    
-    echo "Building kernel image..."
     make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-    
-    echo "Building device tree blobs..."
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
@@ -60,7 +52,6 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# === [TODO: Create necessary base directories] ===
 mkdir -p ${OUTDIR}/rootfs
 cd ${OUTDIR}/rootfs
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr/bin usr/lib usr/sbin var/log
@@ -71,48 +62,37 @@ then
     git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # === [TODO: Configure busybox] ===
     make distclean
     make defconfig
 else
     cd busybox
 fi
 
-# === [TODO: Make and install busybox] ===
-echo "Building and installing BusyBox..."
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
-# 回到 rootfs 目錄
 cd ${OUTDIR}/rootfs
-
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# === [TODO: Add library dependencies to rootfs] ===
-# 從交叉編譯器的 sysroot 中找到需要的函式庫並複製
+# 搬運函式庫
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
-echo "Sysroot path: ${SYSROOT}"
-
 cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 lib/
 cp ${SYSROOT}/lib64/libm.so.6 lib64/
 cp ${SYSROOT}/lib64/libresolv.so.2 lib64/
 cp ${SYSROOT}/lib64/libc.so.6 lib64/
 
-# === [TODO: Make device nodes] ===
-echo "Creating device nodes..."
+# 建立設備節點
 sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 600 dev/console c 5 1
 
-# === [TODO: Clean and build the writer utility] ===
-echo "Building writer application..."
+# 編譯 writer
 cd ${FINDER_APP_DIR}
 make clean
 make CROSS_COMPILE=${CROSS_COMPILE}
 
-# === [TODO: Copy the finder related scripts and executables to home] ===
-echo "Copying scripts to rootfs/home..."
+# 複製腳本與程式
 cp writer ${OUTDIR}/rootfs/home/
 cp finder.sh ${OUTDIR}/rootfs/home/
 cp finder-test.sh ${OUTDIR}/rootfs/home/
@@ -121,16 +101,10 @@ mkdir -p ${OUTDIR}/rootfs/home/conf
 cp conf/username.txt ${OUTDIR}/rootfs/home/conf/
 cp conf/assignment.txt ${OUTDIR}/rootfs/home/conf/
 
-# 修正腳本路徑引用 (A3 要求將 ../conf 改為 conf)
+# 修改 finder-test.sh 裡面的路徑
 sed -i 's|\.\./conf|conf|g' ${OUTDIR}/rootfs/home/finder-test.sh
 
-# === [TODO: Chown the root directory] ===
-echo "Setting ownership to root..."
+# 設定權限與打包
 cd ${OUTDIR}/rootfs
 sudo chown -R root:root *
-
-# === [TODO: Create initramfs.cpio.gz] ===
-echo "Creating initramfs..."
 find . | cpio -H newc -ov --owner root:root | gzip > ${OUTDIR}/initramfs.cpio.gz
-
-echo "DONE! Kernel Image and initramfs are in ${OUTDIR}"
