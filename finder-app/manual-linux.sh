bfb1a668ac8f9b12c4b009fb8bbcc2b944100ffd
nckuyc <h44131221@gs.ncku.edu.tw>
2026-02-24 17:13:09 +0800

    Update manual-linux.sh to correct CROSS_COMPILE variable and enhance kernel build steps

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -11,8 +11,8 @@ KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
-# 修正編譯器名稱以符合多數容器環境
-CROSS_COMPILE=aarch64-linux-gnu-
+# 注意：在官方測試容器中，通常使用這個名稱
+CROSS_COMPILE=aarch64-none-linux-gnu-
 
 if [ $# -lt 1 ]
 then
@@ -26,18 +26,24 @@ mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
+    #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
+
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # Build steps
+    # TODO: Add your kernel build steps here
+    echo "Cleaning kernel..."
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    echo "Configuring kernel..."
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    echo "Building kernel Image..."
     make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    echo "Building DTB..."
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
@@ -52,6 +58,7 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
+# TODO: Create necessary base directories
 mkdir -p ${OUTDIR}/rootfs
 cd ${OUTDIR}/rootfs
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr/bin usr/lib usr/sbin var/log
@@ -62,12 +69,14 @@ then
     git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
+    # TODO:  Configure busybox
     make distclean
     make defconfig
 else
     cd busybox
 fi
 
+# TODO: Make and install busybox
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
@@ -76,23 +85,24 @@ echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# 搬運函式庫
+# TODO: Add library dependencies to rootfs
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 lib/
 cp ${SYSROOT}/lib64/libm.so.6 lib64/
 cp ${SYSROOT}/lib64/libresolv.so.2 lib64/
 cp ${SYSROOT}/lib64/libc.so.6 lib64/
 
-# 建立設備節點
+# TODO: Make device nodes
 sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 600 dev/console c 5 1
 
-# 編譯 writer
+# TODO: Clean and build the writer utility
 cd ${FINDER_APP_DIR}
 make clean
 make CROSS_COMPILE=${CROSS_COMPILE}
 
-# 複製腳本與程式
+# TODO: Copy the finder related scripts and executables to the /home directory
+# on the target rootfs
 cp writer ${OUTDIR}/rootfs/home/
 cp finder.sh ${OUTDIR}/rootfs/home/
 cp finder-test.sh ${OUTDIR}/rootfs/home/
@@ -101,10 +111,12 @@ mkdir -p ${OUTDIR}/rootfs/home/conf
 cp conf/username.txt ${OUTDIR}/rootfs/home/conf/
 cp conf/assignment.txt ${OUTDIR}/rootfs/home/conf/
 
-# 修改 finder-test.sh 裡面的路徑
+# 修改路徑引用以符合作業要求
 sed -i 's|\.\./conf|conf|g' ${OUTDIR}/rootfs/home/finder-test.sh
 
-# 設定權限與打包
+# TODO: Chown the root directory
 cd ${OUTDIR}/rootfs
 sudo chown -R root:root *
+
+# TODO: Create initramfs.cpio.gz
 find . | cpio -H newc -ov --owner root:root | gzip > ${OUTDIR}/initramfs.cpio.gz
